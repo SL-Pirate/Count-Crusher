7e2efaf4f9c39da525a0a55d5679369cfd91717a
SL-Pirate <isiraherath626@gmail.com>
2023-03-05 23:39:39 +0530

    First page done!

--- src/main/java/lk/ac/iit/countcrusher/MainCtrl.java
+++ src/main/java/lk/ac/iit/countcrusher/MainCtrl.java
@@ -1,5 +1,15 @@
 package lk.ac.iit.countcrusher;
 
-public class MainCtrl{
+//import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
+public class MainCtrl{
+    @FXML
+    private TextArea inputField;
+
+    private String[] getItems () {
+        String unformattedTxt = inputField.getText();
+        return unformattedTxt.split("\\r?\\n");
+    }
 }
